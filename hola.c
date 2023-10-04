9934650a4cc9b15143fe3a2694cac264b800e97e
Juan Manuel Patiño <juan.patino@udla.edu.ec>
2023-10-04 16:52:35 -0500

    Cuarta linea de codigo

--- hola.c
+++ hola.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
-
-int main (void) 
+int main () 
 {
-    printf("Hola mundo. \n");
-    printf("Hola el mejor mundo. \n");
-    printf("Hello el mejor mundo. \n");
+    int numero;
+    int contador = 0;
+    int continuar;
+    do
+    {
+        printf("Ingrese un numero.\n");
+        scanf("%d", &numero);
+        for (int i = 1; i <= numero; i++)
+        {
+            if (numero % i == 0)
+            {
+                contador++;
+            }
+        }
+        if (contador > 2) 
+        {
+            printf (" El numero %d no es primo. \n", numero);
+        }
+        else 
+        {
+            printf ("El numeor %d es primo. \n", numero);
+        }
+        printf ("Deseas continuar? 1=Si, 0=No");
+        scanf ("%d", &continuar);
+        if (continuar == 1)
+        {
+            contador=0;
+        }
+    } while (continuar==1);
     return 0;
 }
